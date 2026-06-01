6bc3ced285ffb53a2e845a72c1ea6577acd159bf
M Syihabuddin Zuhri <syihabzuhri301004@gmail.com>
2026-06-01 20:29:09 +0700

    nothing here

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'https://divinity-humbling-mantra.ngrok-free.dev'),
 
     /*
     |--------------------------------------------------------------------------
